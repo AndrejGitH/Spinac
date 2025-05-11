016287ac61647a42b1adcc5f3727308e80fcc1eb
Andrej <andrej.chupan@gmail.com>
2025-05-11 14:57:45 +0200

    Error handling and  def strcture added

--- Spinac.py
+++ Spinac.py
@@ -1,6 +1,8 @@
-#1. Download XML config from linux
-#2. Change element light from true to false in the xml config file
-#3. Upload xml file back to the linux
+#1. User enters credentials as parameters
+#2. Try connet to the server by SSH
+#3. Try download the file via sftp
+#4. Try update and sent file back via sftp
+#5. Close connection
 
 
 #Library allowing us to connect to the linux server
@@ -8,16 +10,19 @@ import paramiko
 #Module to allow us work with xml file
 import xml.etree.ElementTree as ET
 
-#Connection variables
+#Connection parameters - user provided
 hostname = input("Enter hostname/IP adress: ")
-port = 22
 user = input("Enter username: ")
 password = input("Enter password: ")
+linuxXML_path = input("Enter Linux file path: ")
+local_path = input("Enter local file path: ")
+port = 22
 
+def process_xml_file(hostname, user, password, linuxXML_path, local_path, port=22):
 
 try: 
 
-    #Connection logic
+    #Connecting to the server via SSH
     client = paramiko.SSHClient()
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -25,14 +30,24 @@ try:
 
     print("SSH connection established!")
 
-#Parsing and updating the xml config file
+except Exception as e:
+    print("Connection failed:", e)
+    exit()
+
+#XML downloading
+try:
     sftp = client.open_sftp()
-    linuxXML_path = input("Enter Linux file path: ")
-    local_path = input("Enter local file path: ")
-    
     sftp.get(linuxXML_path, local_path)
     print("XML downloaded!")
+except FileNotFoundError:
+    print("XML file not found!")
+    exit()
+except Exception as ex:
+    print("File download failed! Details: ", ex)
+    exit()
 
+#Updating the file and sending to the linux server via sftp
+try:
     tree = ET.parse(local_path)
     root = tree.getroot()
 
@@ -52,8 +67,17 @@ try:
     else:
         print("No light element with true value found!")
 
+except FileNotFoundError:
+    print("Local path is wrong! No XML found.")
+    exit()
+except ET.ParseError:
+    print("Error occured while parsing the file!")
+    exit()
+except Exception as exc:
+    print("Error occured, details: ", exc)
+    exit()
+
+    
+finally:
     sftp.close()
     client.close()
-
-except Exception as e:
-    print("Connection failed,:", e)
